66bcbf2acaf5f3d8dbb46395e0b194db02435052
Oles Shulzhenko <simplemanua@gmail.com>
2024-10-27 11:53:56 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -53,7 +53,7 @@ class MovieViewSet(viewsets.ModelViewSet):
         if title:
             queryset = queryset.filter(title__icontains=title)
 
-        if self.action in ["list", "retrieve"]:
+        if self.action == ("list", "retrieve"):
             queryset = Movie.objects.prefetch_related("genres", "actors")
 
         return queryset.distinct()
